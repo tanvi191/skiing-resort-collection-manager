d2f15ca708ddbb0925b2b10300cac427bebebfdd
Tanvi <tanvidav6@gmail.com>
2025-01-14 11:58:13 +0000

    added remove resort function, fetches and displays a list of ski resorts, allows user to remove a resort and updates the state to reflect that change

---
+++ src/components/RemoveResort.tsx
@@ -0,0 +1,40 @@
+import React, { useState, useEffect } from 'react';
+import { useNavigate } from 'react-router-dom';
+import { skiResortService } from '../services/SkiResortService';
+import { SkiResort } from '../types/SkiResort';
+
+const RemoveResort: React.FC = () => {
+    const [resorts, setResorts] = useState<SkiResort[]>([]);
+    const navigate = useNavigate();
+
+    useEffect(() => {
+        const fetchResorts = async () => {
+            const fetchedResorts = await skiResortService.fetchSkiResorts();
+            setResorts(fetchedResorts);
+        };
+        fetchResorts();
+    }, []);
+
+    const handleRemove = (id: number) => {
+        skiResortService.removeSkiResort(id);
+        setResorts(resorts.filter(resort => resort.id !== id));
+    };
+
+    return (
+        <div className="remove-resort">
+            <h2>Remove Ski Resort</h2>
+            <ul>
+                {resorts.map(resort => (
+                    <li key={resort.id}>
+                        {resort.Resort} - {resort.Country}
+                        <button onClick={() => handleRemove(resort.id)}>Remove</button>
+                    </li>
+                ))}
+            </ul>
+            <button onClick={() => navigate('/resorts')}>Back to Resorts</button>
+        </div>
+    );
+};
+
+export default RemoveResort;
+
