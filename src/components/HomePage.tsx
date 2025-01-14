fb0c7ae20f55d07ed5b22c63f4551402ebfbada1
Tanvi <tanvidav6@gmail.com>
2025-01-14 11:40:22 +0000

    added buttons so user can navigate to other pages

--- src/components/HomePage.tsx
+++ src/components/HomePage.tsx
@@ -8,11 +8,17 @@ const HomePage: React.FC = () => {
             <h1>Ski Resort Manager</h1>
             <div className="world-map-container">
                 <img src={worldMap} alt="World Map" className="world-map" />
-                <div className="map-icon" style={{ top: '30%', left: '48%' }}>
                 {/* Add more icons for other countries */}
+            </div>
+            <div className="options">
+                <Link to="/resorts" className="option-button">View All Resorts</Link>
+                <Link to="/dashboard" className="option-button">View Dashboard</Link>
+                <Link to="/add-resort" className="option-button">Add New Resort</Link>
+                <Link to="/remove-resort" className="option-button">Remove Resort</Link>
             </div>
         </div>
     );
 };
 
 export default HomePage;
+
