52948b40726f2c82eb334bb2bc0c529eada44c7b
Tanvi <tanvidav6@gmail.com>
2025-01-14 13:44:56 +0000

    updated to include animation background

--- src/components/HomePage.tsx
+++ src/components/HomePage.tsx
@@ -1,17 +1,20 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
-import worldMap from '../assets/world-map.png';
+// import worldMap from '../assets/world-map.png';
 
 const HomePage: React.FC = () => {
     return (
         <div className="home-page">
             <h1>Ski Resort Manager</h1>
             <div className="world-map-container">
-                <img src={worldMap} alt="World Map" className="world-map" />
-                <div className="map-icon" style={{ top: '30%', left: '48%' }}>
+                <video autoPlay loop muted className="world-map-video">
+                    <source src="/3593892521-preview.mp4" type="video/mp4" />
+                    Your browser does not support the video tag.
+                </video>
+                <div className="map-icon" style={{ top: '40%', left: '48%' }}>
                     <Link to="/resorts?country=Austria">🏔️</Link>
                 </div>
-                <div className="map-icon" style={{ top: '32%', left: '46%' }}>
+                <div className="map-icon" style={{ top: '38%', left: '46%' }}>
                     <Link to="/resorts?country=Belgium">🏔️</Link>
                 </div>
                 {/* Add more icons for other countries */}
@@ -27,4 +30,6 @@ const HomePage: React.FC = () => {
 };
 
 export default HomePage;
+
+
 
